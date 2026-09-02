95b7ec5baf3dd5dd291415d1e827118b053fe7db
beulah08122006 <beulahmariyadurai@gmail.com>
2026-09-02 17:10:39 +0530

    Connect frontend to production backend

--- pharmacy-frontend-v2/src/services/api.ts
+++ pharmacy-frontend-v2/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5050/api",
+  baseURL: "https://pharmacy-management-system-2pwy.onrender.com/api",
 
   headers: {
     "Content-Type": "application/json",
